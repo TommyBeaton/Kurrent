22e699a2134c2f52e242790e95ead1eb174ea8f9
Tommy Beaton <tbeaton88@googlemail.com>
2023-10-30 23:20:25 +0000

    remove unused directives

--- src/Implementation/Notifiers/SlackNotifier.cs
+++ src/Implementation/Notifiers/SlackNotifier.cs
@@ -1,8 +1,6 @@
 using System.Net;
 using System.Net.Http.Headers;
-using System.Text.Encodings.Web;
 using System.Text.Json;
-using System.Xml;
 using Kurrent.Models.Data;
 using Kurrent.Models.Data.Notifiers;
 using Kurrent.Utils;
